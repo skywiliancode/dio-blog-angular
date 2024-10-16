8e1b06a45cd99ec40b293754b0a7e22b555f2f64
skywiliancode <codewilian852@gmail.com>
2024-10-16 14:35:24 -0300

    blog angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component'
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent,BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
