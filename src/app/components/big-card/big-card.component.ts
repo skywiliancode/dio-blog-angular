b6e5a1d069a3088f3839d39ec14e27e292829f5d
skywiliancode <codewilian852@gmail.com>
2024-11-07 15:29:04 -0300

    Alterando para passar os dados dinâmicamente

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.scss'
 })
 export class BigCardComponent implements OnInit{
 
+  @Input()id: string = '0'
   @Input()photoCover: string = ''
   @Input()cardTitle: string = ''
   @Input()cardDescription: string = ''
