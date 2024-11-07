a42927b7dda90132967dc6654aa3eebd00f31321
skywiliancode <codewilian852@gmail.com>
2024-11-07 15:30:31 -0300

    Fazendo alteração

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.scss'
 })
 export class SmallCardComponent implements OnInit{
 
+  @Input()id: string = '0'
   @Input()photoCover: string = ''
   @Input()cardTitle: string = ''
 
